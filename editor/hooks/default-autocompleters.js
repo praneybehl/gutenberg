122bc280c5af07649eb43b9ebb9edca95555fa32
Riad Benguella <benguella@gmail.com>
2018-06-01 13:31:06 +0100

    Shared Blocks: Fix editing paragraph blocks

--- editor/hooks/default-autocompleters.js
+++ editor/hooks/default-autocompleters.js
@@ -24,14 +24,6 @@ function setDefaultCompleters( completers, blockName ) {
 		// Add blocks autocompleter for Paragraph block
 		if ( blockName === getDefaultBlockName() ) {
 			completers.push( clone( blockAutocompleter ) );
-
-			/*
-			 * NOTE: This is a hack to help ensure shared blocks are loaded
-			 * so they may be included in the block completer. It can be removed
-			 * once we have a way for completers to Promise options while
-			 * store-based data dependencies are being resolved.
-			 */
-			dispatch( 'core/editor' ).fetchSharedBlocks();
 		}
 	}
 	return completers;
@@ -42,3 +34,13 @@ addFilter(
 	'blocks/autocompleters/set-default-completers',
 	setDefaultCompleters
 );
+
+/*
+ * NOTE: This is a hack to help ensure shared blocks are loaded
+ * so they may be included in the block completer. It can be removed
+ * once we have a way for completers to Promise options while
+ * store-based data dependencies are being resolved.
+ */
+setTimeout( () => {
+	dispatch( 'core/editor' ).fetchSharedBlocks();
+} );
